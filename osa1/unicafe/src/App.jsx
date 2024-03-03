807070982c55864d9365782d08b6e33278183e02
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-03-03 18:52:44 +0200

    1.6 unicafe step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const handleGoodClick = ()  => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = ()  => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = ()  => {
+    setBad(bad + 1)
+  }
+
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
+      
+      <h1>Statistics</h1>
+
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
+
