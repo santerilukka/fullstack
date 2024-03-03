0394487bc848f06129f99cd8a02ffa905de4aa23
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-03-03 19:26:58 +0200

    1.7 unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -4,20 +4,24 @@ import { useState } from 'react'
 const App = () => {
   const handleGoodClick = ()  => {
     setGood(good + 1)
+    setTotal(total + 1)
   }
 
   const handleNeutralClick = ()  => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBadClick = ()  => {
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   return (
     <div>
@@ -32,6 +36,11 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+
+      <p>Total: {total} </p>
+      <p>Average: {(good - bad) / total}</p>
+      <p>Positive: {(good / total) * 100} %</p>
+      <p></p>
     </div>
   )
 }
