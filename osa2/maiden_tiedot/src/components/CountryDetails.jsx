c8991b2229554f802a7e87d155101b7107ce9f76
santerilukka <156800715+santerilukka@users.noreply.github.com>
2024-03-23 17:20:33 +0200

    2.20 maiden tiedot step3

--- osa2/maiden_tiedot/src/components/CountryDetails.jsx
+++ osa2/maiden_tiedot/src/components/CountryDetails.jsx
@@ -1,13 +1,54 @@
-const CountryDetails = ({ country }) => (
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const CountryDetails = ({ country }) => {
+    const [weather, setWeather] = useState(null)
+    const apiKey = import.meta.env.VITE_SOME_KEY
+    
+    useEffect(() => {
+        const fetchWeather = async () => {
+          try {
+            const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}&units=metric`
+            )
+            setWeather(response.data);
+          } catch (error) {
+            console.error('Error fetching weather data:', error)
+          }
+        }
+        if (country.capital) {
+          fetchWeather()
+        }
+      }, [country.capital, apiKey])
+
+    return(
     <div>
-      <h2>{country.name.common}</h2>
-      <img src={country.flags.png} alt={country.flags.alt} />
-      <p><strong>Languages:</strong> {Object.values(country.languages).join(', ')}</p>
-      <p><strong>Capital:</strong> {country.capital || "Not provided"}</p>
-      <p><strong>Region:</strong> {country.region}</p>
-      <p><strong>Population:</strong> {country.population}</p>
-      <p><strong>Area:</strong> {country.area}</p>
+      <div>
+        <h2>{country.name.common}</h2>
+        
+        <img src={country.flags.png} alt={country.flags.alt} />
+        
+        <p><strong>Languages:</strong> {Object.values(country.languages).join(', ')}</p>
+        <p><strong>Capital:</strong> {country.capital || "Not provided"}</p>
+        <p><strong>Region:</strong> {country.region}</p>
+        <p><strong>Population:</strong> {country.population}</p>
+        <p><strong>Area:</strong> {country.area}</p>
+      </div>
+      <br />
+      <div>
+      {weather && (
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <p><strong>Temperature:</strong> {weather.main.temp} °C</p>
+          <p><strong>Weather:</strong> {weather.weather[0].description}</p>
+          {weather.weather[0].icon && (
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />)}
+          <p><strong>Wind:</strong> {weather.wind.speed} m/s</p>
+        </div>
+      )}
+      </div>
     </div>
-  )
+    )
+}
 
 export default CountryDetails
